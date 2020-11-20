a4e07e9a3e9b9753fbe8c739bd179df19ac23523
Alessandra Laudando <sandral2277@gmail.com>
2020-11-20 10:12:15 -0600

    Added HW 1

---
+++ Semester I/Homework/Homework1/Instructions.dart
@@ -0,0 +1,15 @@
+/* 
+  1) Write a function called myName.
+     myName should print out your name.
+*/
+
+/* 
+  2) Write a function named favoriteAnimal that has these parameters:
+      name, scientificName, origin.
+      favoriteAnimal should print out name, scientificName, origin.
+*/
+
+/* 
+  3) Take the function you created in step 2 (favoriteAnimal) and make 
+     scientificName a named optional parameter
+*/
