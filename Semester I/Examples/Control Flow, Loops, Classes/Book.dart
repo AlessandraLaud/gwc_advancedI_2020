90028c38062237d9f06382a1f739dab7519c7071
Alessandra Laudando <sandral2277@gmail.com>
2021-02-11 15:57:39 -0600

    examples edit

--- Semester I/Examples/Control Flow, Loops, Classes/Book.dart
+++ Semester I/Examples/Control Flow, Loops, Classes/Book.dart
@@ -1,29 +1,8 @@
 class Book {
-  String title;
-  double price;
-  String author;
-  int numberOfPages;
+  // attributes here:
 
-  // constructor
-  Book(this.title, this.price, this.author, this.numberOfPages);
+  // constructor here:
+  Book() {}
 
-  // Book({ this.title, @required this.price, this.author, this.numberOfPages });
-
-  double salesTax() {
-    // sales tax rate is 6%
-    return (6 / 100) * this.price;
-  }
-}
-
-class Ebook extends Book {
-  String website;
-
-  Ebook(this.website, title, price, author, numberOfPages)
-      : super(title, price, author, numberOfPages);
-
-  @override
-  double salesTax() {
-    // sales tax rate is 5% for an ebook
-    return (5 / 100) * this.price;
-  }
+  // behaviors/methods here:
 }
---
+++ Semester I/Examples/Control Flow, Loops, Classes/Ebook.dart
@@ -0,0 +1,12 @@
+// Inheritance Example
+
+import 'Book.dart';
+
+class Ebook extends Book {
+  // attributes here:
+
+  // constructor here:
+  Ebook() {}
+
+  // behaviors/methods here:
+}
